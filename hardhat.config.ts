ab2aa172dce5e90d594cb7e0da02627a2401453d
Sumit Ghosh <sumit@sumit-ghosh.com>
2022-07-22 00:26:31 +0530

    Publish typechain types to NPM

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,9 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  typechain: {
+    outDir: "typechain-types/src",
+  },
 };
 
 export default config;
