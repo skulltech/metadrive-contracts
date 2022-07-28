c92f14a92f6497f7c20be601527f4669842e11be
Sumit Ghosh <sumit@sumit-ghosh.com>
2022-07-28 20:30:11 +0530

    Store public key of user in the contract

--- contracts/MetadriveFile.sol
+++ contracts/MetadriveFile.sol
@@ -19,38 +19,80 @@ contract MetadriveFile is
 
     Counters.Counter private _tokenIdCounter;
 
-    // For each tokenized file, store the encrypted symmetric key
-    mapping(uint256 => mapping(address => string)) public keys;
+    struct User {
+        string username;
+        bytes32 publicKey;
+    }
 
-    // Checks if the caller is the NFT owner
-    modifier callerIsOwner(uint256 tokenId) {
-        require(
-            msg.sender == ownerOf(tokenId),
-            "Caller does not own the token."
-        );
+    // For each tokenized file, store the encrypted symmetric key
+    mapping(uint256 => mapping(address => string)) public encryptionKeys;
+
+    // Store the user info for each registered address
+    mapping(address => User) public users;
+
+    // Checks if the address is the NFT owner
+    modifier isOwner(uint256 tokenId, address addr) {
+        require(addr == ownerOf(tokenId), "User does not own the file.");
+        _;
+    }
+
+    // Checks if the address is not the NFT owner
+    modifier isNotOwner(uint256 tokenId, address addr) {
+        require(addr != ownerOf(tokenId), "User owns the file.");
+        _;
+    }
+
+    // Checks if the address is an user
+    modifier isUser(address addr) {
+        require(users[addr].publicKey != 0, "User does not exist.");
+        _;
+    }
+
+    // Checks if the address is not an user
+    modifier isNotUser(address addr) {
+        require(users[addr].publicKey == 0, "User already exists.");
         _;
     }
 
     constructor() ERC721("MetadriveFile", "MDF") {}
 
-    function safeMint(string memory uri, string memory key) public {
+    function safeMint(string memory uri, string memory encryptionKey) public {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
         _setTokenURI(tokenId, uri);
-        keys[tokenId][msg.sender] = key;
+
+        // Store the encryptionKey of this file for this user
+        encryptionKeys[tokenId][msg.sender] = encryptionKey;
     }
 
-    function share(uint256 tokenId, address to) public callerIsOwner(tokenId) {
-        delete keys[tokenId][to];
+    // Create a new user
+    function createUser(string memory username, bytes32 publicKey)
+        public
+        isNotUser(msg.sender)
+    {
+        User memory user = User({username: username, publicKey: publicKey});
+        users[msg.sender] = user;
     }
 
-    function unshare(
+    // Share a file i.e. give read access to an user
+    function shareFile(
         uint256 tokenId,
         address to,
-        string memory key
-    ) public callerIsOwner(tokenId) {
-        keys[tokenId][to] = key;
+        string memory encryptionKey
+    ) public isUser(msg.sender) isUser(to) isOwner(tokenId, msg.sender) {
+        encryptionKeys[tokenId][to] = encryptionKey;
+    }
+
+    // Unshare a file i.e. revoke read access from an user
+    function unshareFile(uint256 tokenId, address to)
+        public
+        isUser(msg.sender)
+        isUser(to)
+        isOwner(tokenId, msg.sender)
+        isNotOwner(tokenId, to)
+    {
+        delete encryptionKeys[tokenId][to];
     }
 
     // The following functions are overrides required by Solidity.
