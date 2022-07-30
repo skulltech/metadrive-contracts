29a2fc721f0d84e26e8959d30e4d32603c329c16
Sumit Ghosh <sumit@sumit-ghosh.com>
2022-07-30 12:00:04 +0530

    Store only the public key in contract, remove username

--- contracts/MetadriveFile.sol
+++ contracts/MetadriveFile.sol
@@ -19,80 +19,76 @@ contract MetadriveFile is
 
     Counters.Counter private _tokenIdCounter;
 
-    struct User {
-        string username;
-        bytes32 publicKey;
+    // For each tokenized file, store the encrypted file key
+    mapping(uint256 => mapping(address => string)) public fileKeys;
+
+    // Store the public key for each registered address
+    mapping(address => bytes32) public publicKeys;
+
+    // Checks if the address is registered
+    modifier isRegistered(address addr) {
+        require(publicKeys[addr].length > 0, "Address is not registered.");
+        _;
     }
 
-    // For each tokenized file, store the encrypted symmetric key
-    mapping(uint256 => mapping(address => string)) public encryptionKeys;
-
-    // Store the user info for each registered address
-    mapping(address => User) public users;
+    // Checks if the address is not registred
+    modifier isNotRegistered(address addr) {
+        require(publicKeys[addr].length == 0, "Address is already registered.");
+        _;
+    }
 
     // Checks if the address is the NFT owner
     modifier isOwner(uint256 tokenId, address addr) {
-        require(addr == ownerOf(tokenId), "User does not own the file.");
+        require(addr == ownerOf(tokenId), "Address does not own the file.");
         _;
     }
 
     // Checks if the address is not the NFT owner
     modifier isNotOwner(uint256 tokenId, address addr) {
-        require(addr != ownerOf(tokenId), "User owns the file.");
-        _;
-    }
-
-    // Checks if the address is an user
-    modifier isUser(address addr) {
-        require(users[addr].publicKey != 0, "User does not exist.");
-        _;
-    }
-
-    // Checks if the address is not an user
-    modifier isNotUser(address addr) {
-        require(users[addr].publicKey == 0, "User already exists.");
+        require(addr != ownerOf(tokenId), "Address owns the file.");
         _;
     }
 
     constructor() ERC721("MetadriveFile", "MDF") {}
 
-    function safeMint(string memory uri, string memory encryptionKey) public {
+    function safeMint(string memory uri, string memory fileKey) public {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
         _setTokenURI(tokenId, uri);
 
-        // Store the encryptionKey of this file for this user
-        encryptionKeys[tokenId][msg.sender] = encryptionKey;
+        // Store the encrypted file key of this file for this user
+        fileKeys[tokenId][msg.sender] = fileKey;
     }
 
-    // Create a new user
-    function createUser(string memory username, bytes32 publicKey)
-        public
-        isNotUser(msg.sender)
-    {
-        User memory user = User({username: username, publicKey: publicKey});
-        users[msg.sender] = user;
+    // Register an address
+    function register(bytes32 publicKey) public isNotRegistered(msg.sender) {
+        publicKeys[msg.sender] = publicKey;
     }
 
-    // Share a file i.e. give read access to an user
+    // Share a file i.e. give read access to an address
     function shareFile(
         uint256 tokenId,
         address to,
-        string memory encryptionKey
-    ) public isUser(msg.sender) isUser(to) isOwner(tokenId, msg.sender) {
-        encryptionKeys[tokenId][to] = encryptionKey;
+        string memory fileKey
+    )
+        public
+        isRegistered(msg.sender)
+        isRegistered(to)
+        isOwner(tokenId, msg.sender)
+    {
+        fileKeys[tokenId][to] = fileKey;
     }
 
     // Unshare a file i.e. revoke read access from an user
     function unshareFile(uint256 tokenId, address to)
         public
-        isUser(msg.sender)
-        isUser(to)
+        isRegistered(msg.sender)
+        isRegistered(to)
         isOwner(tokenId, msg.sender)
         isNotOwner(tokenId, to)
     {
-        delete encryptionKeys[tokenId][to];
+        delete fileKeys[tokenId][to];
     }
 
     // The following functions are overrides required by Solidity.
